c704db6130c021a707ae536af4117e69f65bc494
Developer LuisF3 <developerluisfernando3@gmail.com>
2020-09-25 16:19:52 -0500

    [MOD, index.js][ADD, inicializaciónDeProyecto-motorDeRutas]

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Initiliazations
+const app = express();
+
+// Settings
+app.set('views', path.join(__dirname, './src/views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// Middlewares
+
+// Global Variables
+
+// Routes 
+app.use(require('./src/routes/index'));
+
+// Statics Files
+app.use(express.static(path.join(__dirname, './src/statics')));
+
+exports.app = functions.https.onRequest(app);
